89fa7f5b9b675bd5b6a09f7ca14158666db2b674
Arafat0122 <soyebarafat0122@yahoo.com>
2025-12-19 10:37:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <img className="w-96" src="/Rahmah-Institute.png" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
